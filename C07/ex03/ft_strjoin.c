6d96d7b49593928ead9c475b53153f3f2b0a3ca2
Gareti10 <92rafa@gmail.com>
2025-04-23 10:32:31 -0300

    second commit

--- C07/ex03/ft_strjoin.c
+++ C07/ex03/ft_strjoin.c
@@ -3,31 +3,35 @@ char *ft_strjoin(int size, char **strs, char *sep)
 {
 	char	*concatenation;
 	int	i;
+	int	*count;
 
+	count = 0;
 	i = 0;
 	concatenation = malloc(size * int);  
 	if (size <= 0)
 	{
-		concatenation[0] = '\0'
+		concatenation[0] = '\0';
 		return (concatenation);
 	}
 	while (i < size)
 	{
 		ft_strcat(concatenation[i], strs[i]);
 		i++;
-		concatenation[i] = '\0';
+		concatenation[i] = sep;
 	}
 	return (concatenation);
 }
 
-ft_strcat(char *concatenation, char *str)
+ft_strcat(char *concatenation, char *str, int *count)
 {
-	int	i;
+	int	j;
 
-	i = 0;
-	while (str[i])
+	j= 0;
+	while (str[count])
 	{
-		concatenation[i] = str[i];
-		i++;
+		concatenation[count] = str[j];
+		count++;
+		j++;
 	}
+	concatenation[j] = '\0';
 }
